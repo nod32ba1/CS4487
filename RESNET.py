2a58cdb74f5377a4a3d55a0f0c19e67d6b260411
nod32ba1 <52558002+nod32ba1@users.noreply.github.com>
2022-12-01 15:24:37 +0800

    random seed

--- RESNET.py
+++ RESNET.py
@@ -1,4 +1,5 @@
 from glob import glob # For Reading Data
+import os
 import numpy as np # Helps in storing large data in NP Arrays
 import time
 import cv2 # For Image Processing
@@ -39,6 +40,13 @@ def getData():
         if(len(trainX)%(0.1*l)==0):print(f'process: {100*len(trainX)/l}%**')
     return np.asarray(trainX), trainY
 
+os.environ['TF_CUDNN_DETERMINISTIC']= '1'
+os.environ['PYTHONHASHSEED']= '4487'
+np.random.seed(4487)
+random.seed(4487)
+tf.random.set_seed(4487)
+
+
 # with zipfile.ZipFile("data.zip", 'r') as zip_ref:
 #     zip_ref.extractall()
 all_X,all_Y = getData()
@@ -72,7 +80,7 @@ ResNet_model.compile(loss='binary_crossentropy',optimizer=Adam(learning_rate=flo
 early_stopping = EarlyStopping(monitor = 'val_loss', min_delta = 0, patience = 10, verbose = 0, mode = 'min', restore_best_weights=True) # Stop early if the val_loss does not reduce for 5 epochs
 
 checkpoint = ModelCheckpoint("ResNet.h5", monitor='val_accuracy', verbose=1, save_best_only=True, save_weights_only=False, mode='auto') # Save the best model with respect to val_accuracy
-
+tf.random.set_seed(4487)
 ResNet_hist=ResNet_model.fit(trainX,trainY,epochs=int(epoch),batch_size = int(bs),validation_data=(testX,testY), callbacks=[checkpoint, early_stopping]) # Fit the model
 
 
